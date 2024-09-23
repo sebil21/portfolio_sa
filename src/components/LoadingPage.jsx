2fe37f85a922128c4568ee8d861bf2fbc6ff549e
Sebil Alagoz <s.alagoz@outlook.fr>
2024-09-24 00:58:10 +0200

    Création header et footer + scss et création des variables et mixins en cours. Prochaine étape, créer une loadingPage

---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,9 @@
+import '../styles/_loadingPage.scss';
+
+export default function LoadingPage() {
+    return (
+        <div className="loading">
+            <p>Sebil Alagoz - Portfolio</p>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles/main.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
