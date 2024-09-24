6b33d6b7fa12410bf4f71de9284a85c3c4af8892
Sebil Alagoz <s.alagoz@outlook.fr>
2024-09-24 10:43:29 +0200

    Loading Page OK + SCSS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/main.scss';
+import LoadingPage from './components/LoadingPage';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
@@ -11,6 +12,7 @@ const root = createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
+        <LoadingPage />
         <Router>
             <Header />
             <Routes>
