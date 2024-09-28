49f5c89d4f939bf24f2f358aaf5c34e1a6e831ef
Sebil Alagoz <s.alagoz@outlook.fr>
2024-09-28 16:28:31 +0200

    Modification section compétences et de sa mise en page, peut etre rajouter un box shadow ?

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,9 +6,16 @@ const Skills = () => {
         <div id="skills" className="skills">
             <h2 className="skills__title">COMPÉTENCES</h2>
             <div className="skills__container">
-                {skills.map((skill, index) => (
+                {skills.map((category, index) => (
                     <div key={index} className="skills__card">
-                        <p className="skills__text">{skill}</p>
+                        <h3 className="skills__subtitle">{category.title}</h3>
+                        <div className="skills__list">
+                            {category.skills.map((skill, skillIndex) => (
+                                <p key={skillIndex} className="skills__text">
+                                    {skill}
+                                </p>
+                            ))}
+                        </div>
                     </div>
                 ))}
             </div>
--- src/data/SkillsList.jsx
+++ src/data/SkillsList.jsx
@@ -1,14 +1,33 @@
 const skills = [
-    'HTML5',
-    'CSS',
-    'SASS',
-    'JAVASCRIPT',
-    'REACT',
-    'API',
-    'SEO',
-    'FIGMA',
-    'SAP S/4HANA',
-    'Infor M3',
-    'Achats',
+    {
+        title: 'Développement Web',
+        skills: ['HTML5', 'CSS', 'SASS', 'Mobile First'],
+    },
+    {
+        title: 'Développement',
+        skills: ['React', 'API', 'JavaScript', 'GitHub'],
+    },
+    {
+        title: 'Référencement',
+        skills: ['SEO', 'Lighthouse', 'Search Console'],
+    },
+    {
+        title: 'Gestion de projet',
+        skills: [
+            'Figma',
+            'Spéc techniques',
+            'Kanban',
+            'SASS',
+            'Veille',
+            'SAP S/4Hana',
+            'Infor M3',
+            'Formation ERP',
+        ],
+    },
+    {
+        title: 'Domaine industriel',
+        skills: ['Achats', 'Reporting'],
+    },
 ];
+
 export default skills;
