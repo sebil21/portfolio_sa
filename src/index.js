b311cce871444821ec74175c415381c986261986
Sebil Alagoz <s.alagoz@outlook.fr>
2024-10-09 14:10:13 +0200

    modif loading

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const root = createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
-        <LoadingPage />
         <Router>
+            <LoadingPage />
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
